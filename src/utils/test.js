d2f4dff038bb8d2bf6da23b8e8e29434dfb3dc92
516010874 <516010874@qq.com>
2021-06-25 18:41:41 +0800

    'test'

---
+++ src/utils/test.js
@@ -0,0 +1,125 @@
+export const testData = {
+    errno: 0,
+    data: {
+        alarmList: [
+            {
+                id: 64,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625114617.jpg',
+                alarmAddr: ' memo 7号楼8号楼中间走廊166',
+                alarmTime: '2021-06-24 16:18:05',
+                siteId: 100,
+                alarmType: '烟火',
+                status: 2,
+                alarmLevel: 1,
+                deviceY: 0.7517234884968993,
+                deviceX: 0.14286832126453952
+            },
+            {
+                id: 62,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625102318.jpg',
+                alarmAddr: 'memo 9号楼左侧165',
+                alarmTime: '2021-06-24 15:18:05',
+                siteId: 100,
+                alarmType: '未戴安全帽',
+                status: 1,
+                alarmLevel: 0,
+                deviceY: 0.948433993543079,
+                deviceX: 0.7052593947142963
+            },
+            {
+                id: 65,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625114724.jpg',
+                alarmAddr: ' memo 7号楼8号楼中间走廊166',
+                alarmTime: '2021-06-24 15:18:05',
+                siteId: 100,
+                alarmType: '危险入侵',
+                status: 0,
+                alarmLevel: 1,
+                deviceY: 0.13352177538571564,
+                deviceX: 0.6589894102636815
+            },
+            {
+                id: 68,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625141439.jpg',
+                alarmAddr: 'memo 5号楼水池相机164',
+                alarmTime: '2021-06-23 15:38:05',
+                siteId: 100,
+                alarmType: '危险入侵',
+                status: 0,
+                alarmLevel: 1,
+                deviceY: 0.9827829599672518,
+                deviceX: 0.6889016213197208
+            },
+            {
+                id: 67,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625141106.jpg',
+                alarmAddr: 'memo 5号楼右侧走廊相机162',
+                alarmTime: '2021-06-22 16:18:05',
+                siteId: 100,
+                alarmType: '烟火',
+                status: 0,
+                alarmLevel: 1,
+                deviceY: 0.08886849471115865,
+                deviceX: 0.7102690255248092
+            },
+            {
+                id: 70,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625142422.jpg',
+                alarmAddr: 'memo 5号楼右侧走廊相机162',
+                alarmTime: '2021-06-22 13:18:05',
+                siteId: 100,
+                alarmType: '未戴安全帽',
+                status: 0,
+                alarmLevel: 0,
+                deviceY: 0.8665736461949144,
+                deviceX: 0.160747103603186
+            },
+            {
+                id: 69,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625141531.jpg',
+                alarmAddr: 'memo 5号楼右侧走廊相机162',
+                alarmTime: '2021-06-22 12:18:05',
+                siteId: 100,
+                alarmType: '未穿反光衣',
+                status: 0,
+                alarmLevel: 0,
+                deviceY: 0.48483550177091705,
+                deviceX: 0.9551565172728393
+            },
+            {
+                id: 66,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625115410.jpg',
+                alarmAddr: '门口相机163',
+                alarmTime: '2021-06-21 14:18:05',
+                siteId: 100,
+                alarmType: '未穿反光衣',
+                status: 0,
+                alarmLevel: 0,
+                deviceY: 0.2752859346632447,
+                deviceX: 0.13379582211512597
+            },
+            {
+                id: 63,
+                alarmURI:
+                    'https://xuzhi-1305205599.cos.ap-nanjing.myqcloud.com/alarm/20210625112036.jpg',
+                alarmAddr: '门口相机163',
+                alarmTime: '2021-06-21 11:18:05',
+                siteId: 100,
+                alarmType: '未戴安全帽',
+                status: 1,
+                alarmLevel: 0,
+                deviceY: 0.5156669817791729,
+                deviceX: 0.25331371824337134
+            }
+        ]
+    },
+    errmsg: '成功'
+}
